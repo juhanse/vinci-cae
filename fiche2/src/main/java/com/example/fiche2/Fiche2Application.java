1470264765003121d63a832c70abe0604494e912
juhanse <julien.hanse12@gmail.com>
2026-02-11 20:37:07 +0100

    add: fiche2 exercice

---
+++ fiche2/src/main/java/com/example/fiche2/Fiche2Application.java
@@ -0,0 +1,13 @@
+package com.example.fiche2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fiche2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fiche2Application.class, args);
+    }
+
+}
---
+++ fiche2/src/test/java/com/example/fiche2/Fiche2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fiche2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fiche2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
